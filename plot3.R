4294e76408cc60de08e0d57f35661cc6cc9008b6
foricher <christian.foricher@gmail.com>
2014-07-10 19:29:48 +0200

    Update

--- plot3.R
+++ plot3.R
@@ -4,16 +4,15 @@ source("common.R")
 
 data <- get_hpc_data ()
 
+png("plot3.png", width=480, height=480)
 with(data, {
     plot(DateTime, Sub_metering_1, type="l", xlab="", ylab="Global Active Power (kilowatts)",col ="black")
     lines(DateTime,Sub_metering_2,col='Red')
     lines(DateTime,Sub_metering_3,col='Blue')
     legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
-           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),cex=0.5 
-             )
+           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 })
 
-## Save file
-dev.copy(png, file="plot3.png", height=480, width=480, units="px")
+#dev.copy(png, file="plot3.png", height=480, width=480, units="px")
 dev.off()
 
--- plot4.R
+++ plot4.R
@@ -4,6 +4,8 @@ source("common.R")
 
 data <- get_hpc_data ()
 
+png("plot4.png", width=480, height=480)
+
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 
 with(data, {
@@ -15,14 +17,13 @@ with(data, {
     lines(DateTime,Sub_metering_2,col='Red')
     lines(DateTime,Sub_metering_3,col='Blue')
     legend("topright", col=c("black", "red", "blue"), lty=1, lwd=1,  bty="n",
-           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex=0.5, inset=0)
+           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
     
     plot(DateTime, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
     
 })
 
 
-## Save file
-dev.copy(png, file="plot4.png", height=480, width=480, units="px")
+#dev.copy(png, file="plot4.png", height=480, width=480, units="px")
 dev.off()
 
